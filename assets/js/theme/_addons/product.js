7a71b0b01632003e67ca0b21f31d45ccd11e9c42
logan <logan@logans-Mac-mini.local>
2025-10-28 16:00:01 -0700

    Next Stopping Point. Grabbing DOM Elements by key. Standardized DOM content id's. Added constant for the root category.

--- assets/js/theme/_addons/product.js
+++ assets/js/theme/_addons/product.js
@@ -1,20 +1,33 @@
 import PageManager from '../page-manager';
-import { CDN_ROOT } from './product/constants'; 
+import { CDN_ROOT, CATEGORY_ROOT } from './product/constants';
 import inStockNotifyForm from './product/inStockNotify';
 import CsGallery from './product/csGallery';
 
 export default class Product extends PageManager {
 
-  titleData = document.querySelector('h1').dataset; //Gets some properties of the BigCommerce product from the product template
-  isArchetype = this.titleData.productCategory === 'Qty - All Products'; //the category string if the page is an archetype page 
-  qtyDataUrl = this.#buildArchetypeDataUrl();
+  #elementKeys = [
+    'badges', 'blemForm', 'brand', 'description', 'fitmentNotes',
+    'instructions', 'messages', 'more', 'moreHeader', 'price',
+    'shipping', 'sku', 'slides', 'stock', 'title',
+  ];
+  contentElements = {};
+
+  titleData = null;
+  isArchetype = false;
+  qtyDataUrl = null;
   gallery = null;
+  aliasVehicle = null;
 
   constructor(context) {
     super(context);
   }
 
   async onReady() {
+    this.#populateContentElements();
+    this.titleData = this.contentElements.title.dataset;
+    this.isArchetype = this.titleData.productCategory === CATEGORY_ROOT;
+    this.qtyDataUrl = this.#buildArchetypeDataUrl();
+
     this.gallery = new CsGallery();
 
     // Add the Qty Data Script to the page
@@ -24,7 +37,18 @@ export default class Product extends PageManager {
       console.error(error);
     }
 
+    if (!this.isArchetype && !universal_product) { this.aliasVehicle = this.#parseVehicleString(this.titleData.productTitle) };
+
     console.log('last update:', last_update);
+    console.log('alias vehicle:', this.aliasVehicle);
+  }
+
+  #populateContentElements() {
+    this.contentElements = this.#elementKeys.reduce((acc, key) => {
+      const id = 'product-' + key.replace(/[A-Z]/g, (match) => `-${match.toLowerCase()}`);
+      acc[key] = document.getElementById(id);
+      return acc
+    }, {})
   }
 
   #buildArchetypeDataUrl() {
@@ -57,5 +81,38 @@ export default class Product extends PageManager {
 
       document.body.appendChild(script);
     })
+  }
+
+  #parseVehicleString(title) {
+    const findLongestMatch = (searchString, candidates, key) => {
+      if (!searchString || !Array.isArray(candidates) || candidates.length === 0) {
+        return null;
+      }
+
+      const matches = candidates.filter(candidate => {
+        const name = key ? candidate[key] : candidate;
+        return searchString.startsWith(name);
+      });
+
+      if (matches.length === 0) {
+        return null;
+      }
+
+      return matches.reduce((best, current) => {
+        const bestName = key ? best[key] : best;
+        const currentName = key ? current[key] : current;
+        return currentName.length > bestName.length ? current : best;
+      });
+    };
+    const vehicleString = title.replace(og_name + ' for ', '').trim();
+    const make = findLongestMatch(vehicleString, make_data);
+    const modelGenString = vehicleString.replace(make, '').trim();
+    const model = findLongestMatch(modelGenString, model_data[make]);
+    const genString = modelGenString.replace(model, '').replace(' to ', '-').trim();
+    const bestGenMatch = findLongestMatch(genString, gen_data[model], 'name');
+    const gen = bestGenMatch ? bestGenMatch.index : null;
+    const sku = this.contentElements.sku.dataset.productSku;
+
+    return { make, model, gen, sku };
   }
 }
--- assets/js/theme/_addons/product/constants.js
+++ assets/js/theme/_addons/product/constants.js
@@ -1,1 +1,2 @@
 export const CDN_ROOT = 'https://cravenspeed-cdn.sfo3.cdn.digitaloceanspaces.com/';
+export const CATEGORY_ROOT = 'Qty - All Products';
